72502da44fc59a251217e89762f13d35b892ad0d
Carlos <cjworkk42@gmail.com>
2025-03-11 14:32:18 -0300

    questao11.c
    Faça um programa que calcule e mostre a área de um losango. Sabe-se que: A = (diagonal maior * diagonal menor)/2.

---
+++ questao11.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+	int diagonal_maior, diagonal_menor;
+	scanf ("%i%i", &diagonal_maior, &diagonal_menor);
+	printf ("%i", diagonal_maior*diagonal_menor/2);
+	return 0;		
+}
