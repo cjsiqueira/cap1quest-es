b8d2a4465fc2476f2e62f6c43569e915fec13c4e
Carlos <cjworkk42@gmail.com>
2025-03-11 10:12:05 -0300

    questao4.c
    Faça um programa que receba duas notas, calcule e mostre a média ponderada dessas notas, considerando peso 2 para a primeira e peso 3 para a segunda.

---
+++ questao4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	int nota1, nota2;
+	
+  scanf ("%i", &nota1);
+  scanf ("%i", &nota2);
+  printf ("%i", (nota1*2+nota2*3)/5);
+  return 0;
+}
